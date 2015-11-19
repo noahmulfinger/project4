683bd7324596b4ff7a1c6b13214816005351eef1
Noah Mulfinger <noah.mulfinger@gmail.com>
2015-11-19 13:19:46 -0800

    computing camera matrix and displaying calculated points is working

--- main.m
+++ main.m
@@ -1,14 +1,70 @@
 clear all
 close all
 
-% Read in images (center and peripherals)
-resize = .5;
-center = imresize(im2double(imread('./edmunds/center.JPG')), resize);
-per1 = imresize(im2double(imread('./edmunds/top.JPG')), resize);
-per2 = imresize(im2double(imread('./edmunds/bottom.JPG')), resize);
+% Noah's image set:
+% 4177, 4178, 4184, 4186, 4181, 4190
+
+I = im2double(imread('./horse/DSCF4177.jpg'));
+
+X = [ 100 200 200 0;
+      0   40  200 200;
+      0    0   0  0;
+      1    1   1  1];
+  
+
+ 
+P = compute_camera_matrix(I,X);
+
+% a = [100; 60; 0; 1];
+
+C = ones(4,121);
+counter = 1;
+for i = 0:20:200
+    for j = 0:20:200
+        C(1,counter) = i;
+        C(2,counter) = j;
+        C(3,counter) = 0;
+        counter = counter + 1;
+    end
+end
+
+x = P * C;
+x(1,:) = x(1,:) ./ x(3,:);
+x(2,:) = x(2,:) ./ x(3,:);
+x(3,:) = x(3,:) ./ x(3,:);
+
+figure, imshow(I)
+hold on;
+for i = 1:121
+    plot(x(1,i),x(2,i),'Marker','.','Color',[1 0 0],'MarkerSize',20);
+end
+
+
+
+
+
+% center = zeros(3000, 3000);
+% 
+% [X, Y] = meshgrid(1:300:3001, 1:300:3001);
+% 
+% n = 12;
+% w = 640;
+% h = 480;
+% x = [ w 0 ; 0 h ] * rand( 2, n );
+% xmeas = [ round( x + randn( 2, n )) ; ones( 1, n ) ]
+% xpmeas = [ round( xp(1:2,:) + randn( 2, n )); ones( 1, n ) ];
+% return;
+
+
+% for i = 1:3000
+%     for j = 1:3000
+%         if mod(i, 300) == 0 && mod(j, 300) == 0
+%             center(i,j) = 1;
+%         end            
+%     end
+% end
 
 
 
 % Select any number of pairs of points that correspond between images
-[moved, fixed] = cpselect(per, center, 'Wait', true);
-moved = cpcorr(moved, fixed, per, center);
+% moved = cpcorr(moved, fixed, per, center)% moved = cpcorr(moved, fixed, per, center);
