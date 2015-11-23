e7c643109b465f43689c7b660a7aa0df6520c50f
Noah Mulfinger <noah.mulfinger@gmail.com>
2015-11-22 17:07:25 -0800

    created and saved camera matrices for first set of images

--- main.m
+++ main.m
@@ -4,17 +4,22 @@ close all
 % Noah's image set:
 % 4177, 4178, 4184, 4186, 4181, 4190
 
-I = im2double(imread('./horse/DSCF4177.jpg'));
+I = im2double(imread('./horse/DSCF4190.jpg'));
 
-X = [ 100 200 200   0   0  64  64;
-        0  40 200 200   0   0  64;
-        0   0   0   0  19  19  19;
-        1   1   1   1   1   1   1];
+X = [64 0 29 1;
+     64 0 0 1;
+     64 64 29 1;
+     32 48 67 1;
+     32 80 67 1;
+     32 80 29 1;
+     180 20 0 1;
+     180 180 0 1;
+     20 180 0 1]';
 
 
  
-P = compute_camera_matrix(I,X);
-
+P4190 = compute_camera_matrix(I,X);
+P = P4190;
 
 
 C = ones(4,121);
@@ -28,8 +33,49 @@ for i = 0:20:200
     end
 end
 
-% x = P * a;  
-x = P * C;
+
+B = [0, 0, 0;
+64, 0, 0;
+64, 64, 0;
+0, 64, 0;
+0, 0, 19;
+64, 0, 19;
+64, 64, 19;
+0, 64, 19;
+0, 0, 29;
+64, 0, 29;
+64, 64, 29;
+0, 64, 29;
+16, 16, 29;
+48, 16, 29;
+48, 48, 29;
+16, 48, 29;
+16, 16, 48;
+48, 16, 48;
+48, 48, 48;
+16, 48, 48;
+0, 48, 29;
+32, 48, 29;
+32, 80, 29;
+0, 80, 29;
+0, 48, 48;
+32, 48, 48;
+32, 80, 48;
+0, 80, 48;
+0, 48, 48;
+32, 48, 48;
+32, 80, 48;
+0, 80, 48;
+0, 48, 67;
+32, 48, 67;
+32, 80, 67;
+0, 80, 67];
+
+B = cat(2, B, ones(36,1))';
+
+D = cat(2, B, C);
+
+x = P * D;
 x(1,:) = x(1,:) ./ x(3,:);
 x(2,:) = x(2,:) ./ x(3,:);
 x(3,:) = x(3,:) ./ x(3,:);
@@ -37,53 +83,18 @@ x(3,:) = x(3,:) ./ x(3,:);
 
 figure, imshow(I)
 hold on;
-for j = 1:121
-    plot(x(1,j),x(2,j),'Marker','.','Color',[1 0 0],'MarkerSize',20);
+for i = 1:size(x,2)
+    plot(x(1,i),x(2,i),'Marker','.','Color',[1 0 0],'MarkerSize',20);
 end
-% figure, imshow(I)
-% hold on;
-% for i = 1:121
-%     plot(x(1,i),x(2,i),'Marker','.','Color',[1 0 0],'MarkerSize',20);
-% end
-
-
-% a = [32; 48; 48; 1];
-
-% b = apply_camera_matrix(P, a);
-
-
-
-% x(1) = x(1) / x(3);
-% x(2) = x(2) / x(3);
-% x(3) =  1;
-% x
-% plot(x(1),x(2),'Marker','.','Color',[1 0 0],'MarkerSize',20);
 
 
 
 
-% center = zeros(3000, 3000);
-% 
-% [X, Y] = meshgrid(1:300:3001, 1:300:3001);
-% 
-% n = 12;
-% w = 640;
-% h = 480;
-% x = [ w 0 ; 0 h ] * rand( 2, n );
-% xmeas = [ round( x + randn( 2, n )) ; ones( 1, n ) ]
-% xpmeas = [ round( xp(1:2,:) + randn( 2, n )); ones( 1, n ) ];
-% return;
-
-
-% for i = 1:3000
-%     for j = 1:3000
-%         if mod(i, 300) == 0 && mod(j, 300) == 0
-%             center(i,j) = 1;
-%         end            
-%     end
-% end
 
 
 
-% Select any number of pairs of points that correspond between images
-% moved = cpcorr(moved, fixed, per, center)% moved = cpcorr(moved, fixed, per, center);
+
+
+
+
+
